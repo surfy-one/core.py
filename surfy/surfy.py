c0517db8b94f2e113c2805e5b141e07662136637
Alexander Yermolenko <i@surfy.one>
2023-02-11 09:22:50 +0500

    Fixed pylint

--- surfy/surfy.py
+++ surfy/surfy.py
@@ -6,7 +6,27 @@ Surfy
 
 class Surfy:
 
+	'''
+	
+	Global Surfy
+
+	'''
+
 	# Initialisation
 	def __init__(self, options):
 		self.options = options
+
+	def help(self):
+
+		'''
+
+			Get Surfy Help
+
+		'''
+
+		helper = {
+			'SQLite': 'Usefull lib for work with sqlite'
+		}
+
+		print(helper)
 		
--- tests/__main__.py
+++ tests/__main__.py
@@ -4,5 +4,5 @@ Surfy Tests
 
 '''
 
-import init
+from tests import init
 init.test()
